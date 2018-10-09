d84f3e86d7c82068f143c6ad744812e5720435f3
Jayashree Raman <ramankjayashree@gmail.com>
2018-10-08 20:10:40 -0700

    Added comments

--- A2_InitialSetup_DataLoad/postgresql_queries.sql
+++ A2_InitialSetup_DataLoad/postgresql_queries.sql
@@ -1,12 +1,15 @@
 
 
+
+/* Query 3a */
 SELECT * FROM "Track" WHERE "Composer" = 'Angus Young, Malcolm Young, Brian Johnson'
 
+/* Query 3b */
 SELECT "Track"."Name", "Genre"."Name"
 FROM "Track" JOIN "Genre"
 ON "Track"."GenreId" = "Genre"."GenreId"
 
-
+/* Query 3c */
 SELECT "T"."Genre Name", Avg("T"."UnitPrice") As "AvgUnitPrice"
 FROM
 (SELECT "Track"."Name" as "Track Name", "Genre"."Name" as "Genre Name", "Track"."UnitPrice", "Track"."Milliseconds"
