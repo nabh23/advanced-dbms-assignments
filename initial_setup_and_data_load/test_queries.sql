e2252a4fa43b452871cde3d50d7a09e2705a274e
Harkar Talwar <harkar.talwar@gmail.com>
2018-10-08 21:56:05 -0700

    Project Milestone: Initial Setup and Data Load | Updating test queries

--- initial_setup_and_data_load/test_queries.sql
+++ initial_setup_and_data_load/test_queries.sql
@@ -1,21 +1,32 @@
-
-
-
 /* Query 3a */
-SELECT * FROM "Track" WHERE "Composer" = 'Angus Young, Malcolm Young, Brian Johnson'
+SELECT * 
+FROM "Track" 
+WHERE "Milliseconds" > 200000;
 
 /* Query 3b */
-SELECT "Track"."Name", "Genre"."Name"
-FROM "Track" JOIN "Genre"
-ON "Track"."GenreId" = "Genre"."GenreId"
+WITH "T1" AS (
+	SELECT * 
+	FROM "Track" 
+	WHERE "Milliseconds" > 200000
+)
+SELECT "T1".*, "M1"."Name" as "MediaType"
+FROM "T1" JOIN "MediaType" "M1"
+ON "T1"."MediaTypeId" = "M1"."MediaTypeId";
 
 /* Query 3c */
-SELECT "T"."Genre Name", Avg("T"."UnitPrice") As "AvgUnitPrice"
-FROM
-(SELECT "Track"."Name" as "Track Name", "Genre"."Name" as "Genre Name", "Track"."UnitPrice", "Track"."Milliseconds"
-FROM "Track" JOIN "Genre"
-ON "Track"."GenreId" = "Genre"."GenreId") as "T"
-Where "T"."Milliseconds" > 800000
-GROUP BY
-"Genre Name"
-ORDER BY "Genre Name"
+WITH "T2" AS (
+	WITH "T1" AS (
+		SELECT * 
+		FROM "Track" 
+		WHERE "Milliseconds" > 200000
+	)
+	SELECT "T1".*, "M1"."Name" as "MediaType"
+	FROM "T1" JOIN "MediaType" "M1"
+	ON "T1"."MediaTypeId" = "M1"."MediaTypeId"
+)
+SELECT "MediaType" as "MediaType", round(Avg("UnitPrice"), 2) as "AvgSongPrice"
+FROM "T2"
+GROUP BY "MediaType"
+ORDER BY "AvgSongPrice";
+
+
